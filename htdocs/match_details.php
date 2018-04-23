ea6df8a6da8d0692c2e118c8af5a653540d1385f
optimus88 <keshav_optimus88@outlook.com>
2018-04-24 02:33:14 +0530

    Message

--- htdocs/match_details.php
+++ htdocs/match_details.php
@@ -118,19 +118,20 @@ if(isset($_POST['submit_match_details_selection']))
             </div>
 		</div>
 		<!-- end #content -->
-		<div id="sidebar">
+        <?php include ('sidebar_teamLogo.php'); ?>
+		<!--<div id="sidebar">
 			<ul>
                 <li>
 					<h2>News Updates :</h2>
 					<ul>
-                        <li><img src="images/teamLogo/delhi.png"/>VS<img src="images/teamLogo/guj.png"/></li>
+                        <li><img src="images/teamLogo/delhi.png"/><img src="images/teamLogo/guj.png"/></li>
                         <li><img src="images/teamLogo/kkr.png"/><img src="images/teamLogo/mi.png"/></li>
                         <li><img src="images/teamLogo/pune.png"/><img src="images/teamLogo/punjab.png"/></li>
                         <li><img src="images/teamLogo/rcb.png"/><img src="images/teamLogo/srh.png"/></li>
                    </ul>
                 </li>
 			</ul>
-		</div>
+		</div> -->
 		<!-- end #sidebar -->
 		<div style="clear: both;">&nbsp;</div>
 	</div>
