1b811b39fed78d206cd8bb073f78584bc6c10469
optimus88 <keshav_optimus88@outlook.com>
2018-04-24 02:30:50 +0530

    sidebar logo file...

---
+++ htdocs/sidebar_teamLogo.php
@@ -0,0 +1,13 @@
+<div id="sidebar">
+			<ul>
+                <li>
+					<h2>News Updates :</h2>
+					<ul>
+                        <li><img src="images/teamLogo/delhi.png"/><img src="images/teamLogo/raj_logo.png"/></li>
+                        <li><img src="images/teamLogo/kkr.png"/><img src="images/teamLogo/mi.png"/></li>
+                        <li><img src="images/teamLogo/csk.png"/><img src="images/teamLogo/punjab.png"/></li>
+                        <li><img src="images/teamLogo/rcb.png"/><img src="images/teamLogo/srh.png"/></li>
+                   </ul>
+                </li>
+			</ul>
+</div>
